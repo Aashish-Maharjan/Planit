7a9bd29915ff54c111306a4ba711c1787afe277a
Aashish-Maharjan <mhzaashish@gmail.com>
2024-11-14 23:02:36 +0545

    "Added new routes for tasks, users, trash, and task details pages in App.jsx"

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,32 @@
 import Login from './pages/Login.jsx';
 import { Routes,Route,Navigate,Outlet,useLocation } from 'react-router-dom';
 import Dashboard from "./pages/Dashboard.jsx";
+import Tasks from "./pages/Tasks.jsx"; 
+import Users from "./pages/Users.jsx";
+import Trash from "./pages/Trash.jsx";
+import TaskDetails from "./pages/TaskDetails.jsx"; 
 
 function App() {
   return(
   <main className="w-full min-h-screen bg-[#f3f4f6]">
     <Routes> 
       <Route element={<Layout/>}>
+
         <Route path='/' element={<Navigate to="/dashboard"/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
+
+        <Route path='/tasks' element={<Tasks/>}/>
+        <Route path='/completed/:status >' element={<Tasks/>}/>
+        <Route path='/in-progress/:status >' element={<Tasks/>}/>
+        <Route path='/todo/:status >' element={<Tasks/>}/>
+
+        <Route path='/team' element={<Users/>}/>
+
+        <Route path='/trashed' element={<Trash/>}/>
+
+        <Route path='/tasks/:id >' element={<TaskDetails/>}/>
+
+
       </Route>
     </Routes>
   </main>
